46fbff1882355b3bf39e73ebdba34d0fb6fb486b
Piriyan Karu <44897187+ipiriyan2002@users.noreply.github.com>
2023-07-01 20:59:38 +0100

    Update dataset_ids.py

--- dataset_lib/dataset_ids.py
+++ dataset_lib/dataset_ids.py
@@ -1,10 +1,10 @@
 from dataset_lib.ing_dataset import IngDataset
-#from dataset_lib.cord_dataset import CordDataset
+from dataset_lib.cord_dataset import CordDataset
 from dataset_lib.meme_dataset import MemeDataset
 from dataset_lib.glosat_dataset import GlosatDataset
 ids = dict(
     ing=IngDataset,
-    #cordv2=CordDataset,
+    cordv2=CordDataset,
     meme=MemeDataset,
     glosat=GlosatDataset
 )
